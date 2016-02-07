6c692a1c3a7089a07701772a1449e5f6b2ed28ba
Paul Johnson <paul@snuberry-software.co.uk>
2016-02-07 18:23:46 +0000

    Initial import

---
+++ ConnectionPlugin/ConnectionPlugin.cs
@@ -0,0 +1,46 @@
+﻿using Plugin.Connectivity.Abstractions;
+using Xamarin.Forms;
+using Plugin.Connectivity;
+
+namespace ConnectionPlugin
+{
+    public class App : Application
+    {
+        public event ConnectivityChangedEventHandler ConnectivityChanged;
+
+        public bool IsConnected { get; set; } = CrossConnectivity.Current.IsConnected;
+
+        public static App Self { get; set; }
+
+        public App()
+        {
+            App.Self = this;
+
+            // as the connection needs to be handled at the "highest" point of the app, place the
+            // event in the App singleton. In this example, I broadcast a message that anywhere in the app can hook into
+
+            ConnectivityChanged += (object sender, ConnectivityChangedEventArgs e) =>
+            {
+                MessagingCenter.Send<App, string>(this, "Connection", e.IsConnected ? "Connected" : "Not connected");
+            };
+
+            MainPage = new NavigationPage(new StatePage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
+
---
+++ ConnectionPlugin/StatePage.cs
@@ -0,0 +1,59 @@
+﻿using Xamarin.Forms;
+
+namespace ConnectionPlugin
+{
+    public class StatePage : ContentPage
+    {
+        Label connected;
+
+        // always put anything to do with MessagingCenter in OnAppearing and OnDisappearing
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MessagingCenter.Subscribe<App, string>(this, "Connection", (s, e) =>
+            {
+                // the message has been received, so change the text. It is on the UI thread not out of it.
+                Device.BeginInvokeOnMainThread(() => connected.Text = e);
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Unsubscribe<App, string>(this, "Connection");
+        }
+
+        public StatePage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            if (Device.OS == TargetPlatform.iOS)
+                Padding = new Thickness(0, 20, 0, 0);
+
+            var state = new Label
+            {
+                Text = "You are currently",
+                FontSize = 18,
+                TextColor = Color.Red
+            };
+            connected = new Label
+            {
+                Text = App.Self.IsConnected ? "Connected" : "Not Connected",
+                FontSize = 18,
+                TextColor = Color.Blue
+            };
+
+            Content = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    state, connected
+                }
+            };
+        }
+    }
+}
+
+
